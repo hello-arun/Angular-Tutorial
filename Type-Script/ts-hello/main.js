c7b35fe1fbb3dbf7b5889afb1614fecabce8b2a1
arunjangir <jangirarun786@gmail.com>
2022-03-01 19:48:21 +0300

    type-assertion-arrrow-fun

--- Type-Script/ts-hello/main.js
+++ Type-Script/ts-hello/main.js
@@ -1,34 +1,20 @@
-// As we know all this code will get compiled without any error
-// but Type script warns us about the bad habit.
-var a = 5;
-a = "Hello"; // Not valid in TypScript
-// If we do not know value ahead of time 
-// This is acceptabel but it also creates probelem
-var b;
-b = 1;
-b = true;
-b = 'a';
-// Whats the solution ??
-// Here is the sol
-var c;
-c = 1;
-c = true; // You can see it shows error warning  
-// All different types of variables
-var aa;
-var bb;
-var cc;
-var dd;
-var ee = [1, 2, 3];
-var ff = [1, true, 'a'];
-// Bad habit
-var ColorRed = 0;
-var ColorGreen = 1;
-var ColorBlue = 2;
-// Good habit
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 0] = "Red";
-    Color[Color["Green"] = 1] = "Green";
-    Color[Color["Blue"] = 2] = "Blue";
-})(Color || (Color = {}));
-var backgroundColor = Color.Blue; // We get intellisense here
+var message = 'abc';
+message.endsWith('c'); // beautifull intellisense
+// Type Assertions
+var msg; // Any type 
+msg = 'abc'; // later declared as a string
+var endsWithC;
+endsWithC = msg.endsWith('c'); // No intellisense
+endsWithC = msg.endsWith('c'); // beautiful intellisense
+endsWithC = msg.endsWith('c'); // beautiful intellisense
+//Arrow Functions
+var log = function (message) {
+    console.log(message);
+};
+// Simple
+var doLog = function (message) {
+    console.log(message);
+    // more lines here
+};
+// For single line Simpler
+var singleDoLog = function (message) { return console.log(message); };
--- Type-Script/ts-hello/main.ts
+++ Type-Script/ts-hello/main.ts
@@ -1,36 +1,25 @@
-// As we know all this code will get compiled without any error
-// but Type script warns us about the bad habit.
+let message = 'abc'
+message.endsWith('c') // beautifull intellisense
 
-var a = 5
-a = "Hello" // Not valid in TypScript
-// If we do not know value ahead of time 
-// This is acceptabel but it also creates probelem
-var b
-b = 1
-b = true
-b = 'a'
-// Whats the solution ??
-// Here is the sol
-var c
-c = 1
-c = true // You can see it shows error warning  
+// Type Assertions
+let msg; // Any type 
+msg = 'abc' // later declared as a string
+let endsWithC;
+endsWithC = msg.endsWith('c') // No intellisense
+endsWithC = (<string>msg).endsWith('c') // beautiful intellisense
+endsWithC = (msg as string).endsWith('c') // beautiful intellisense
 
-// All different types of variables
-var aa: number
-var bb: boolean
-var cc: string
-var dd: any
-var ee: number[] = [1, 2, 3]
-var ff: any[] = [1, true, 'a']
 
-// Bad habit
-const ColorRed = 0
-const ColorGreen = 1
-const ColorBlue = 2
+//Arrow Functions
 
-// Good habit
-enum Color { Red, Green, Blue }
-let backgroundColor = Color.Blue  // We get intellisense here
+let log = function(message) {
+    console.log(message)
+}
+// Simple
+let doLog = (message) => {
+    console.log(message)
+    // more lines here
+}
 
-// See the main.js file to get feel how enum works in javascript
-// You will instantly rise in love
+// For single line Simpler
+let singleDoLog = (message) => console.log(message)
