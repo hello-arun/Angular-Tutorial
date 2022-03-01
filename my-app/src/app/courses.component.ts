54c9dd9f74e977f9c65bce6f71d272cfdfdcd3ae
arunjangir <jangirarun786@gmail.com>
2022-03-01 23:04:07 +0300

    Directives added

--- my-app/src/app/courses.component.ts
+++ my-app/src/app/courses.component.ts
@@ -2,8 +2,20 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'courses', // div class=> . id=>#
-    template: '<h2>Courses</h2>'
+    template: `
+    <h2>{{"Title: "+title}}</h2>
+    <ul>
+        <li *ngFor="let course of courses">
+        {{course}}
+        </li>
+    </ul>
+    `
+    /* 
+    ` ` are used for multi line input
+    *ngFor is some kind of loop function in Angular
+    */
 })
 export class CoursesComponent {
-
+    title = "List of courses"
+    courses = ["Course1", "Course2", "Course3"]
 }
